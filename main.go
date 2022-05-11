09610e6e1626c066b09d047762ee2648d428d470
R2D2Dan <Danil150799@gmail.com>
2022-05-11 16:21:30 +0300

    add pint

--- main.go
+++ main.go
@@ -11,44 +11,48 @@ func main() {
 	IngotChanel := make(chan string, 5)
 	Done := make(chan int)
 
-	go Find(Mine, MetalChanel)
-	go Melt(MetalChanel, IngotChanel)
-	go Ingot(IngotChanel, Done)
+	go Find(&Mine, &MetalChanel)
+	go Melt(&MetalChanel, &IngotChanel)
+	go Ingot(&IngotChanel, &Done)
 
 	<-Done
+
 }
 
-func Find(Mine [5]string, MetalChanel chan string) {
+func Find(Mine *[5]string, MetalChanel *chan string) {
 	for _, m := range Mine {
 		if m == "ore" {
 			log.Printf("Find: ore")
-			MetalChanel <- "ore"
+			*MetalChanel <- "ore"
 		}
 	}
+	close(*MetalChanel)
 	log.Println("Fine: Done")
-	close(MetalChanel)
+
 }
 
-func Melt(MetalChanel chan string, IngotChanel chan string) {
+func Melt(MetalChanel *chan string, IngotChanel *chan string) {
 
-	for m := range MetalChanel {
+	for m := range *MetalChanel {
 		if m == "ore" {
 			log.Println("Melt: ore -> metal")
-			IngotChanel <- "metal"
+			*IngotChanel <- "metal"
 		}
 	}
+	close(*IngotChanel)
 	log.Println("Melt: Done")
-	close(IngotChanel)
+
 }
 
-func Ingot(IngotChanel chan string, Done chan int) {
+func Ingot(IngotChanel *chan string, Done *chan int) {
 
-	for m := range IngotChanel {
+	for m := range *IngotChanel {
 		if m == "metal" {
 			log.Println("Ingot: metal -> ingot")
 		}
 	}
 	log.Println("Ingot: Done")
-	Done <- 0
+
+	*Done <- 0
 
 }
